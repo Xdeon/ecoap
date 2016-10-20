a7d6a9d90378bf311af3b0f162f98ecdc1596391
Xdeon <hongyfeng123@gmail.com>
2016-10-20 07:56:38 -0600

    Change msg exchange definition location

---
+++ include/coap_exchange.hrl
@@ -0,0 +1,17 @@
+-record(exchange, {
+	timestamp = undefined :: non_neg_integer(),
+	expire_time = undefined :: undefined | non_neg_integer(),
+	stage = undefined :: atom(),
+	sock = undefined :: inet:socket(),
+	ep_id = undefined :: coap_endpoint_id(),
+	endpoint_pid = undefined :: pid(),
+	trid = undefined :: {in, non_neg_integer()} | {out, non_neg_integer()},
+	handler_sup = undefined :: pid(),
+	receiver = undefined :: undefined | {reference(), pid()},
+	msgbin = undefined :: undefined | binary(),
+	timer = undefined :: undefined | reference(),
+	retry_time = undefined :: undefined | non_neg_integer(),
+	retry_count = undefined :: undefined | non_neg_integer()
+	}).
+
+-type coap_exchange() :: #exchange{}.
