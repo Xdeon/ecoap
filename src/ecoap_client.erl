884ae6d4b271780f1cb9de123aa75601209251ae
Xdeon <hongyfeng123@gmail.com>
2016-11-18 02:06:27 -0600

    clean code in coap_client

--- src/ecoap_client.erl
+++ src/ecoap_client.erl
@@ -4,6 +4,7 @@
 %% API.
 -export([start_link/0]).
 -export([ping/2, request/3, request/4, request/5, request_async/3, request_async/4, request_async/5, close/1]).
+% -export([observe/2, observe/3, unobserve/2, unobserve/3]).
 
 %% gen_server.
 -export([init/1]).
@@ -15,7 +16,7 @@
 
 -record(state, {
 	sock_pid = undefined :: pid(),
-	request_refs = undefined :: map(),
+	request_refs = undefined :: #{reference() => req()},
 	client_pid = undefined :: pid()
 }).
 
@@ -33,6 +34,7 @@
 -include("coap_def.hrl").
 
 -type from() :: {pid(), term()}.
+-type req() :: #req{}.
 -type response() :: {ok, atom(), coap_content()} | {error, atom()} | {error, atom(), coap_content()} | {separate, reference()}.
 -type payload() :: coap_message_utils:payload().
 -opaque state() :: #state{}.
@@ -173,17 +175,17 @@ handle_response(Ref, EndpointPid, Message=#coap_message{code={ok, Code}, options
 		undefined -> {noreply, State};
 		Req = #req{method=Method, option_list=OptionList, fragment=Fragment, client_ref=ClientRef, from=From} ->
 			case coap_message_utils:get_option('Block2', Options) of
-		            {Num, true, Size} ->
-		                % more blocks follow, ask for more
-		                % no payload for requests with Block2 with NUM != 0
-		                {ok, Ref2} = coap_endpoint:send(EndpointPid,
-		                	coap_message_utils:request('CON', Method, <<>>, append_option({'Block2', {Num+1, false, Size}}, OptionList))),
-		                {noreply, State#state{request_refs = store_ref(Ref2, Req#req{fragment = <<Fragment/binary, Data/binary>>}, delete_ref(Ref, Refs))}};
-		            _Else ->
-		                % not segmented
-		                Res = return_response({ok, Code}, Message#coap_message{payload= <<Fragment/binary, Data/binary>>}),
-		                ok = send_response(From, ClientPid, ClientRef, Res),
-		                {noreply, State#state{request_refs = delete_ref(Ref, Refs)}}
+	            {Num, true, Size} ->
+	                % more blocks follow, ask for more
+	                % no payload for requests with Block2 with NUM != 0
+	                {ok, Ref2} = coap_endpoint:send(EndpointPid,
+	                	coap_message_utils:request('CON', Method, <<>>, append_option({'Block2', {Num+1, false, Size}}, OptionList))),
+	                {noreply, State#state{request_refs = store_ref(Ref2, Req#req{fragment = <<Fragment/binary, Data/binary>>}, delete_ref(Ref, Refs))}};
+	            _Else ->
+	                % not segmented
+	                Res = return_response({ok, Code}, Message#coap_message{payload= <<Fragment/binary, Data/binary>>}),
+	                ok = send_response(From, ClientPid, ClientRef, Res),
+	                {noreply, State#state{request_refs = delete_ref(Ref, Refs)}}
 		    end
 	end;
 handle_response(Ref, _EndpointPid, Message=#coap_message{code={error, Code}}, 
@@ -195,6 +197,26 @@ handle_response(Ref, _EndpointPid, Message=#coap_message{code={error, Code}},
 		    ok = send_response(From, ClientPid, ClientRef, Res),
 			{noreply, State#state{request_refs = delete_ref(Ref, Refs)}}
 	end.
+
+% send_notify(Ref, Req=#req{lastseq=LastSeq}, Obstate, Message=#coap_message{code={ok, Code}},
+%         State=#state{client_pid=ClientPid, request_refs=Refs}) ->
+%     case Obstate of
+%         undefined ->
+%             % subscription is terminated
+%             ClientPid ! {coap_notify, self(), undefined, {ok, Code}, coap_message_utils:get_content(Message)},
+%             {stop, normal, State};
+%         N when LastSeq == undefined; N > LastSeq ->
+%             % report and stay subscribed
+%             ClientPid ! {coap_notify, self(), N, {ok, Code}, coap_message_utils:get_content(Message)},
+%             {noreply, State#state{request_refs = store_ref(Ref, Req#req{lastseq=N}, Refs)}};
+%         N when N =< LastSeq ->
+%             % ignore, but stay subscribed
+%             {noreply, State}
+%     end;
+% send_notify(_Ref, _Req, _Obstate, Message=#coap_message{code={error, Code}},
+% 	State=#state{client_pid=ClientPid}) ->
+% 	ClientPid ! {coap_notify, self(), undefined, {error, Code}, coap_message_utils:get_content(Message)},
+%     {stop, normal, State}.
 	
 call_endpoint(Pid, Msg) ->
 	gen_server:call(Pid, Msg, ?EXCHANGE_LIFETIME).
@@ -215,7 +237,7 @@ delete_ref(Ref, Refs) ->
 	maps:remove(Ref, Refs).
 
 send_response(undefined, ClientPid, ClientRef, Res) ->
-    ClientPid ! {ClientRef, Res},
+    ClientPid ! {async_response, self(), ClientRef, Res},
     ok;
 send_response(From, _, _, Res) ->
     _ = gen_server:reply(From, Res),
