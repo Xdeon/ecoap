5d4cbcea5b393e9adb72b1d9fc64bd4b7a866f44
Xdeon <hongyfeng123@gmail.com>
2016-11-25 14:02:23 -0600

    Upgrade erlang.mk

--- erlang.mk
+++ erlang.mk
@@ -16,7 +16,7 @@
 
 ERLANG_MK_FILENAME := $(realpath $(lastword $(MAKEFILE_LIST)))
 
-ERLANG_MK_VERSION = 2016.11.03-7-g14b92a1
+ERLANG_MK_VERSION = 2016.11.03-8-g5b67a32
 
 # Make 3.81 and 3.82 are deprecated.
 
