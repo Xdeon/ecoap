9439aa7f8b67244679ad4dea5fb1a044dad760d2
Xdeon <hongyfeng123@gmail.com>
2017-01-12 20:34:11 -0600

    Add comments for benchmarking notes in benchmark.erl

--- src/benchmark.erl
+++ src/benchmark.erl
@@ -58,9 +58,22 @@ coap_unobserve({state, _Prefix, _Name}) ->
 handle_info(_Message, State) -> {noreply, State}.
 coap_ack(_Ref, State) -> {ok, State}.
 
+% we consider non-tail-recursive fibonacci function as a CPU intensive task
+% thus when we do benchmarking, it should be tested combined with ordinary resource
+% e.g. 1000 concurrent clients in total 
+% with 100 requesting "fibonacci?n=30" resource (clients generating CPU load on server, a.k.a., intensive clients) 
+% and 900 requesting "benchmark" resource (clients looking for non-CPU intensice resource, a.k.a., ordinary clients)
+% we observe throughput & latency of the ordinary clients under this situation to test erlang's soft-realtime performance
+% we also need to change PROCESSING_DELAY in coap_exchange.erl to a larger number, e.g. 100s, 
+% to avoid triggering separate response
+
 % fib(0) -> 0;
 % fib(1) -> 1;
 % fib(N) -> fib(N - 1) + fib(N - 2).
+
+% fibnacci function below should be used in any real world case, as it is far more efficient than the last one
+% because of erlang's way of modeling numbers, it is easy to request for a quiet large fib number and the result
+% can be automatically transferred in blocks if its size exceeds the required one
 
 fib(N) -> fib_iter(N, 0, 1).
 
