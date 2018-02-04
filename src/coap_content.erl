eec1a312cd31e0681dd4347ecd6a4e6f01d01375
Xdeon <hongyfeng123@gmail.com>
2018-02-04 01:59:36 -0600

    Add more helper functions in coap_content.erl and fix the problem that ebin/ is not included in tracking files

--- src/coap_content.erl
+++ src/coap_content.erl
@@ -1,5 +1,5 @@
 -module(coap_content).
--export([new/0, new/1, new/2, set_payload/2, set_options/2, get_payload/1, get_options/1]).
+-export([new/0, new/1, new/2, set_payload/2, get_payload/1, set_options/2, get_options/1, set_option/3, get_option/2, get_option/3]).
 -export([get_content/1]).
 
 -include("coap_content.hrl").
@@ -16,11 +16,17 @@ new(Payload, Options) -> #coap_content{payload=Payload, options=Options}.
 
 set_payload(Payload, Content) -> Content#coap_content{payload=Payload}.
 
-set_options(Options, Content) -> Content#coap_content{options=Options}.
-
 get_payload(#coap_content{payload=Payload}) -> Payload.
 
+set_options(Options, Content) -> Content#coap_content{options=Options}.
+
 get_options(#coap_content{options=Options}) -> Options.
+
+set_option(Option, Value, Content=#coap_content{options=Options}) -> Content#coap_content{options=Options#{Option=>Value}}.
+
+get_option(Option, Content) -> get_option(Option, Content, undefined).
+
+get_option(Option, #coap_content{options=Options}, Default) -> maps:get(Option, Options, Default).
 
 get_content(Request) ->
     #coap_content{payload=coap_message:get_payload(Request), options=filter_options(coap_message:get_options(Request))}.
