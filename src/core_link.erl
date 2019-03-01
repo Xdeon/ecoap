f9331c0112733a9b86b075b3a80231a5ce92345e
Xdeon <hongyfeng123@gmail.com>
2019-03-01 13:54:25 +0800

    Formatting

--- src/core_link.erl
+++ src/core_link.erl
@@ -1,13 +1,4 @@
-%
-% The contents of this file are subject to the Mozilla Public License
-% Version 1.1 (the "License"); you may not use this file except in
-% compliance with the License. You may obtain a copy of the License at
-% http://www.mozilla.org/MPL/
-%
-% Copyright (c) 2015 Petr Gotthard <petr.gotthard@centrum.cz>
-%
-
-% encoding and decoding for the CoRE link format, see RFC 6690
+% PALCE FOR LICIENSE
 -module(core_link).
 
 -export([decode/1, encode/1]).
