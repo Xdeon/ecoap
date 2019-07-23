adcac4ffafb860a2ff7054d186aa7b02c789d383
Xdeon <hongyfeng123@gmail.com>
2019-07-23 17:00:31 +0800

    Revert: setting trap_exit in socket process leads to error during termination

--- src/ecoap_dtls_socket.erl
+++ src/ecoap_dtls_socket.erl
@@ -65,13 +65,11 @@ callback_mode() ->
 	state_functions.
 
 init([accept, Name, ListenSocket, ProtoConfig, TimeOut]) ->
-	process_flag(trap_exit, true),
 	StateData = #data{protocol_config=ProtoConfig, server_name=Name, lsocket=ListenSocket, timeout=TimeOut},
 	{ok, accept, StateData, [{next_event, internal, accept}]};
 init([connect, Onwer, EpAddr={PeerIP, PeerPortNo}, TransOpts, ProtoConfig, TimeOut]) ->
 	case ssl:connect(PeerIP, PeerPortNo, ecoap_socket:socket_opts(dtls, TransOpts), TimeOut) of
 		{ok, Socket} ->
-			process_flag(trap_exit, true),
 			ok = ssl:setopts(Socket, [{active, ?ACTIVE_PACKETS}]),
 			EpID = {{dtls, self()}, EpAddr},
 			{ok, connected, #data{protocol_config=ProtoConfig, socket=Socket, ep_id=EpID, timeout=TimeOut}, [{next_event, internal, {connected, Onwer}}]};
