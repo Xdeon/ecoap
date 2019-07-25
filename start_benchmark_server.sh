ef06852b9d42b000c2ae5d4415f834f274b537e2
Xdeon <hongyfeng123@gmail.com>
2019-07-25 22:13:00 +0800

    Update benchmark script

--- start_benchmark_server.sh
+++ start_benchmark_server.sh
@@ -1,3 +1,3 @@
 # !/bin/sh
 gmake 
-erl -pa ./ebin -pa ./deps/*/ebin +K true +spp true -sname benchmark -s benchmark start
+erl -pa ./ebin -pa ./deps/*/ebin +K true +spp true -name benchmark@127.0.0.1 -setcookie ecoap -s benchmark start
