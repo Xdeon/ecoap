b1fdb853912996149d66a317ad9d7d797991e619
Xdeon <hongyfeng123@gmail.com>
2019-12-24 22:34:57 -0600

    Add helper code for starting sample DTLS client in src/example/benchmark.erl

--- src/example/benchmark.erl
+++ src/example/benchmark.erl
@@ -1,6 +1,7 @@
 -module(benchmark).
 -export([coap_discover/1, coap_get/4, coap_post/4]).
--export([start/0, start_dtls/1, stop/0, psk_ciphers/0]).
+-export([start/0, start_dtls/1, stop/0, stop_dtls/0]).
+-export([start_dtls_client/3]).
 -export([fib/1]).
 
 -behaviour(ecoap_handler).
@@ -33,6 +34,18 @@ start_dtls(cert) ->
                     ssl:cipher_suites(anonymous, 'dtlsv1.2') ++ 
                     ssl:cipher_suites(anonymous, 'tlsv1.2')}
     ], #{routes => routes()}).
+
+% client sample code for dtls connection
+start_dtls_client(Host, Port, psk) ->
+    application:ensure_all_started(ssl),
+    ecoap_client:open(Host, Port, 
+        #{transport_opts => psk_options("ecoap.id", #{<<"ecoap.id">> => <<"ecoap.pwd">>})});
+start_dtls_client(Host, Port, cert) ->
+    application:ensure_all_started(ssl),
+    ecoap_client:open(Host, Port, #{transport_opts => 
+        [{ciphers, ssl:cipher_suites(all, 'dtlsv1.2') ++ 
+                    ssl:cipher_suites(anonymous, 'dtlsv1.2') ++ 
+                    ssl:cipher_suites(anonymous, 'tlsv1.2')}]}).
 
 routes() ->
     [
@@ -67,6 +80,11 @@ user_lookup(psk, ClientPSKID, _UserState = PSKs) ->
     {ok, ServerPickedPSK}.
 
 stop() ->
+    ecoap:stop_udp(benchmark_udp),
+    application:stop(ecoap).
+
+stop_dtls() ->
+    ecoap:stop_dtls(benchmark_dtls),
     application:stop(ecoap).
 
 % resource operations
