31b8022cc3916b8d0163baed1c83781bf2e6e36f
Xdeon <hongyfeng123@gmail.com>
2020-11-08 18:01:06 -0600

    Fix strat_benchmark_server.sh

---
+++ examples/benchmark_udp/start_benchmark_server.sh
@@ -0,0 +1,3 @@
+# !/bin/sh
+gmake 
+erl -pa ./ebin -pa ./deps/*/ebin +K true +spp true -name benchmark@127.0.0.1 -setcookie ecoap -s benchmark_udp_app start
--- start_benchmark_server.sh
+++
@@ -1,3 +0,0 @@
-# !/bin/sh
-gmake 
-erl -pa ./ebin -pa ./deps/*/ebin +K true +spp true -name benchmark@127.0.0.1 -setcookie ecoap -s benchmark start
